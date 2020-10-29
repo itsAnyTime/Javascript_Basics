a0729f134e4b1fd91cc255bae9ba853673aacf79
itsanytime <any@itsanytime.de>
2020-10-29 12:33:57 +0100

    edabit objects solution part 1

--- own/edabit.js
+++ own/edabit.js
@@ -225,3 +225,205 @@ const negate = (arr) => {
 console.log(negate([1, 2, 3, 4])); // [-1, -2, -3, -4]
 console.log(negate([-1, 2, -3, 4])); // [1, -2, 3, -4]
 console.log(negate([])); // []
+
+console.log("---------OBJECTS-------");
+console.log("------------A-----Object Zugriff--------");
+
+
+// A
+// Volume of a Box
+// Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box.
+
+// object
+volumeOfBox({ 
+  width: 2, 
+  length: 5, 
+  height: 1 
+}); 
+
+function volumeOfBox(sizes) {
+  return sizes.length * sizes.width * sizes.height;
+} // return value 10
+
+// or with output in function
+function volumeOfBox({ width, height, length}) {
+  let result = width*height*length;
+  console.log("output", result); // output 10. 2x, because 2 objects
+  return result;
+  } 
+
+// object with function output
+console.log("output 2: ", volumeOfBox({ 
+  width: 2, 
+  length: 5, 
+  height: 1 
+})); // 10
+
+
+// or alone
+const volumeOfBox2 = ({ width, length, height }) => console.log(width * length * height);
+volumeOfBox2({ width: 2, length: 3, height: 5 }) // 30
+
+
+console.log("------------B-----Object Zugriff--------");
+
+// B. Extract City Facts
+// Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties: name // population // continent
+
+function cityFacts(city) {
+  let output = city.name + " has a population of " + city.population + " and is situated in " + city.continent;
+  console.log(output); // v
+  return output;
+}
+
+cityFacts({
+  name: "Paris",
+  population: "2,140,526",
+  continent: "Europe"
+}) // "Paris has a population of 2,140,526 and is situated in Europe"
+
+cityFacts({
+  name: "Tokyo",
+  population: "13,929,286",
+  continent: "Asia"
+}) // "Tokyo has a population of 13,929,286 and is situated in Asia"
+
+console.log("------------C-----Object Zugriff: ---------Math.round-");
+
+// // C
+// Calculate the Profit
+// You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. You are given an object containing the ///   cost price per unit (in dollars), 
+//   sell price per unit (in dollars), and the 
+//   starting inventory.
+// Return the total profit made, rounded to the nearest dollar.
+// Notes:
+//  Assume all inventory has been sold.
+//  Profit = Total Sales - Total Cost
+
+function profit(info) {
+  //return console.log((info.inventory * info.sellPrice)-(info.inventory * info.costPrice)); // 54000 gewinn - 39204
+  // shorter:
+  return console.log(Math.round(info.inventory * (info.sellPrice-info.costPrice))); // needs to be rounded in this case
+}
+profit({
+  costPrice: 32.67,
+  sellPrice: 45.00,
+  inventory: 1200
+}) // 14796
+
+profit({
+  costPrice: 225.89,
+  sellPrice: 550.00,
+  inventory: 100
+}) // 32411
+
+profit({
+  costPrice: 2.77,
+  sellPrice: 7.95,
+  inventory: 8500
+}) // 44030
+
+
+console.log("------------D-----Object Zugriff: str in obj------hasOwnProperty----------.includes------------.call-------.some------.keys--");
+
+// D. 
+// Does the Object Contain a Given Key?
+// Write a function that returns true if a hash contains the specified key, and false otherwise.
+
+// .hasOwnProperty
+function hasKey(obj, str) {
+    return console.log(obj.hasOwnProperty(str));
+}
+
+// or in
+function hasKey(obj, str) {
+    return console.log(str in obj);
+}
+
+// or Object / .includes
+function hasKey(obj, key) {
+	return console.log(Object.keys(obj).includes(key));
+}
+
+// or ternary
+const hasKey2 = (obj, key) => obj[key]? true : false;  // output im return
+
+// or ternary with console.log other syntax
+function hasKey(obj, key) {
+	return console.log(obj[key] ? true : false);
+}
+
+// or .call
+function hasKey(obj, key) {
+	return console.log(hasOwnProperty.call(obj,key));
+}
+
+// or .some
+function hasKey(obj, key) {
+	return Object.keys(obj).some(item => item === key); // output im return
+}
+
+// or .keys
+function hasKey(obj, key) {
+	return console.log(Object.keys(obj).includes(key));
+}
+
+hasKey({ a: 44, b: 45, c: 46 }, "d") // false
+hasKey({ craves: true, midnight: true, snack: true }, "morning") // false
+hasKey({ pot: 1, tot: 2, not: 3 }, "not") // true
+
+
+console.log("------------E------");
+
+// Planetary Weight Converter
+// In this challenge, you have to convert a weight weighed on a planet of the Solar System to the corresponding weight on another planet.
+// To convert the weight, you have to divide it by the gravitational force of the planet on which is weighed and multiply the result (the mass) for the gravitational force of the other planet. See the table below for a list of gravitational forces:
+
+// weight on planetA / gravitational force of planetA * gravitational force of planetB
+// Planet	m/s²
+// Mercury	3.7
+// Venus	8.87
+// Earth	9.81
+// Mars	3.711
+// Jupiter	24.79
+// Saturn	10.44
+// Uranus	8.69
+// Neptune	11.15
+
+// Given a weight weighed on planetA, return the converted value for planetB rounded to the nearest hundredth.
+
+function spaceWeights(params) {
+  
+}
+
+spaceWeights("Earth", 1, "Mars") // 0.38
+spaceWeights("Earth", 1, "Jupiter") // 2.53
+spaceWeights("Earth", 1, "Neptune") // 1.14
+
+console.log("------------F------");
+
+
+// International Greetings
+// Suppose you have a guest list of students and the country they are from, stored as key-value pairs in an object.
+
+const GUEST_LIST = {
+  Randy: "Germany",
+  Karla: "France",
+  Wendy: "Japan",
+  Norman: "England",
+  Sam: "Argentina"
+}
+
+// Write a function that takes in a name and returns a name tag, that should read:
+
+function greeting(name) {
+  return "Hi! I'm " + GUEST_LIST.name + " and I'm from " + GUEST_LIST.name(GUEST_LIST.name);
+}
+
+// "Hi! I'm [name], and I'm from [country]."
+// If the name is not in the object, return:
+// "Hi! I'm a guest."
+
+console.log(greeting("Randy")); // "Hi! I'm Randy, and I'm from Germany."
+console.log(greeting("Sam")); // "Hi! I'm Sam, and I'm from Argentina."
+console.log(greeting("Monti"));  //"Hi! I'm a guest."
